f334c68a7f1ee19eba395cc1464a360edd00d7a0
ewnd9 <ewndnine@gmail.com>
2019-06-10 23:11:38 +0300

    add generic and virtual document creation examples

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import {MarkdownLinkProvider} from './MarkdownLinkProvider';
 import {CatCodingPanel} from './TodoPreview';
+import { Editor } from './Editor';
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable0 = vscode.commands.registerCommand('extension.helloWorld', () => {
@@ -17,19 +18,33 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable2);
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('catCoding.start', () => {
-      console.log('here')
+    vscode.commands.registerCommand('extension.openWebview', () => {
       CatCodingPanel.createOrShow(context.extensionPath);
     })
   );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('catCoding.doRefactor', () => {
-      if (CatCodingPanel.currentPanel) {
-        CatCodingPanel.currentPanel.doRefactor();
-      }
+    vscode.commands.registerCommand('extension.openTree', async () => {
+      // Editor.open();
+      const uri = vscode.Uri.parse('cowsay:test');
+      const doc = await vscode.workspace.openTextDocument(uri);
+
+      await vscode.window.showTextDocument(doc, { preview: false });
     })
   );
+
+  const myScheme = 'cowsay';
+  const myProvider = new class implements vscode.TextDocumentContentProvider {
+    onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+    onDidChange = this.onDidChangeEmitter.event;
+
+    provideTextDocumentContent(uri: vscode.Uri): string {
+      console.log(uri);
+      return `doc\n\n\ndoc\n\ntest\n\ntest`;
+    }
+  };
+
+  context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(myScheme, myProvider));
 
   if (vscode.window.registerWebviewPanelSerializer) {
     // Make sure we register a serializer in activation event
