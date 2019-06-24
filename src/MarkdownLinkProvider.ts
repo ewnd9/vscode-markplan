945e577dc05c5ef5e721d4d9eb51078679ec1e27
ewnd9 <ewndnine@gmail.com>
2019-06-24 23:09:14 +0300

    fix MarkdownLinkProvider for virtual document

--- src/MarkdownLinkProvider.ts
+++ src/MarkdownLinkProvider.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import * as vscode from 'vscode';
 import lineColumn from 'line-column';
+import { cwd } from './modules/vscode';
 
 // https://github.com/microsoft/vscode/blob/81d7885dc2e9dc617e1522697a2966bc4025a45d/extensions/markdown-language-features/src/features/documentLinkProvider.ts
 // https://github.com/microsoft/vscode/blob/c1c3e5eab0f2fb9e04a32b4fc6473023a9c25697/extensions/markdown-language-features/src/commands/openDocumentLink.ts
@@ -21,9 +22,10 @@ export class MarkdownLinkProvider implements vscode.DocumentLinkProvider {
     let match: RegExpExecArray | null;
     while (match = regEx.exec(source)) {
       const startPosition = liner.fromIndex(match.index);
-      const endPosition = liner.fromIndex(regEx.lastIndex);
-      const rootDir = vscode.workspace.getWorkspaceFolder(document.uri);
-      const destPath = path.resolve(rootDir!.uri.fsPath, match[0].substring(1));
+      const endPosition = liner.fromIndex(
+        regEx.lastIndex === source.length ? (regEx.lastIndex - 1) : regEx.lastIndex
+      );
+      const destPath = path.resolve(cwd(), match[0].substring(1));
       const uri = vscode.Uri.parse(`command:_markdown.openDocumentLink?${encodeURIComponent(JSON.stringify({ path: encodeURIComponent(destPath)}))}`);
       // not working for some reason, probably wrong argument name
       // const uri = vscode.Uri.parse(`command:vscode.open?${encodeURIComponent(JSON.stringify({ resource: vscode.Uri.file(p)}))}`);
