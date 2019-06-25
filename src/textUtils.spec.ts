bd24cac97ad9644bb6eea640e095d970ed80ca6a
ewnd9 <ewndnine@gmail.com>
2019-06-25 23:57:06 +0300

    fix link parsing regex

--- src/textUtils.spec.ts
+++ src/textUtils.spec.ts
@@ -1,10 +1,13 @@
 import { parseLinks } from './textUtils';
 
 jest.mock('./modules/globby');
+jest.mock('./modules/search');
+jest.mock('./modules/vscode');
 
 test('parseLink', () => {
   expect(parseLinks({source: 'test /link.md parsing'})).toMatchObject([{path: '/link.md'}]);
   expect(parseLinks({source: 'test ../link.md parsing'})).toMatchObject([{path: '../link.md'}]);
   expect(parseLinks({source: 'test ./link.md parsing'})).toMatchObject([{path: './link.md'}]);
+  expect(parseLinks({source: 'test ../test/dir/link.md parsing'})).toMatchObject([{path: '../test/dir/link.md'}]);
   expect(parseLinks({source: 'test http://link.md parsing'})).toMatchObject([]);
 });
