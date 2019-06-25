82f7ff27a2356b2e6d7810f29b4007731a3e5a5e
ewnd9 <ewndnine@gmail.com>
2019-06-25 23:28:39 +0300

    fix link parsing regex

--- src/textUtils.spec.ts
+++ src/textUtils.spec.ts
@@ -6,4 +6,5 @@ test('parseLink', () => {
   expect(parseLinks({source: 'test /link.md parsing'})).toMatchObject([{path: '/link.md'}]);
   expect(parseLinks({source: 'test ../link.md parsing'})).toMatchObject([{path: '../link.md'}]);
   expect(parseLinks({source: 'test ./link.md parsing'})).toMatchObject([{path: './link.md'}]);
+  expect(parseLinks({source: 'test http://link.md parsing'})).toMatchObject([]);
 });
--- src/textUtils.ts
+++ src/textUtils.ts
@@ -39,7 +39,7 @@ interface ParseLinksItem {
 }
 
 export function parseLinks({source}: {source: string;}): Array<ParseLinksItem> {
-  const regEx = /(\.)?(\.)?\/.*\.md/g;
+  const regEx = /(\.)?(\.)?(?<!\/)\/[\w\d-_]*\.md/g;
   const result: Array<ParseLinksItem> = [];
 
   let match: RegExpExecArray | null;
